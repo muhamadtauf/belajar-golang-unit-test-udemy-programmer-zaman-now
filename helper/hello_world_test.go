bb01dfe7f50e95e1c2c9f1128f12a28ffc811634
Muhammad Taufik <muhamadtauf@gmail.com>
2023-11-28 16:39:47 +0700

    menggagalkan test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,14 +1,20 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestHelloWorld(t *testing.T) {
+func TestHelloWorldEko(t *testing.T) {
 	result := HelloWorld("Eko")
 
 	if result != "Hello Eko" {
 		//error
-		panic("Result is not 'Hello Eko'")
+		//panic("Result is not 'Hello Eko'")
+		//t.Fail()
+		t.Error("Result must be 'Hello Eko'")
 	}
+	fmt.Println("TestHelloWorldEko Done")
 }
 
 func TestHelloWorldKhannedy(t *testing.T) {
@@ -16,6 +22,9 @@ func TestHelloWorldKhannedy(t *testing.T) {
 
 	if result != "Hello Khannedy" {
 		//error
-		panic("Result is not 'Hello Khannedy'")
+		//panic("Result is not 'Hello Khannedy'")
+		//t.FailNow()
+		t.Fatal("Result must be 'Hello Khannedy'")
 	}
+	fmt.Println("TestHelloWorldKhannedy Done")
 }
