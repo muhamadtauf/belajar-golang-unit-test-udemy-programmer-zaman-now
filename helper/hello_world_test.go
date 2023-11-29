ef579d5cc4db9a63ab8f0714a91dfb0711bec5dc
Muhammad Taufik <muhamadtauf@gmail.com>
2023-11-29 19:50:57 +0700

    assertions

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,8 +2,22 @@ package helper
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Eko")
+	require.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
+	fmt.Println("TestHelloWorld with Require Done")
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Eko")
+	assert.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
+	fmt.Println("TestHelloWorld with Assert Done")
+}
 
 func TestHelloWorldEko(t *testing.T) {
 	result := HelloWorld("Eko")
