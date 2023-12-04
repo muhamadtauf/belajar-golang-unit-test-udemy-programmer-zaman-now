d1e48fffc741f92a8355e22247f46c43dd5191d5
Muhammad Taufik <muhamadtauf@gmail.com>
2023-12-04 16:24:21 +0700

    sub test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -8,6 +8,18 @@ import (
 	"testing"
 )
 
+func TestSubTest(t *testing.T) {
+	t.Run("Eko", func(t *testing.T) {
+		result := HelloWorld("Eko")
+		require.Equal(t, "Hello Eko", result, "Result must be 'Hello Eko'")
+	})
+
+	t.Run("Kurniawan", func(t *testing.T) {
+		result := HelloWorld("Kurniawan")
+		require.Equal(t, "Hello Kurniawan", result, "Result must be 'Hello Kurniawan'")
+	})
+}
+
 func TestMain(m *testing.M) {
 	//before
 	fmt.Println("BEFORE UNIT TEST")
